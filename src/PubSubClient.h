68ce61349acb86bc0e78bad5a19378e157d481b7
Jannik Svensson <tde04014@gmail.com>
2025-11-26 14:46:38 +0100

    replace all boolean with BOOLEAN_TYPE to make it more platform independent + add PC platforms to use functional as well

--- src/PubSubClient.h
+++ src/PubSubClient.h
@@ -75,8 +75,13 @@
 
 // Maximum size of fixed header and variable length size header
 #define MQTT_MAX_HEADER_SIZE 5
+#if !(defined(_WIN32) || defined(__linux__) || defined(__APPLE__))
+#define BOOLEAN_TYPE boolean
+#else
+#define BOOLEAN_TYPE bool
+#endif
 
-#if defined(ESP8266) || defined(ESP32)
+#if defined(ESP8266) || defined(ESP32) || defined(_WIN32) || defined(__linux__) || defined(__APPLE__)
 #include <functional>
 #define MQTT_CALLBACK_SIGNATURE std::function<void(char*, uint8_t*, unsigned int)> callback
 #else
@@ -98,9 +103,9 @@ private:
    bool pingOutstanding;
    MQTT_CALLBACK_SIGNATURE;
    uint32_t readPacket(uint8_t*);
-   boolean readByte(uint8_t * result);
-   boolean readByte(uint8_t * result, uint16_t * index);
-   boolean write(uint8_t header, uint8_t* buf, uint16_t length);
+   BOOLEAN_TYPE readByte(uint8_t * result);
+   BOOLEAN_TYPE readByte(uint8_t * result, uint16_t * index);
+   BOOLEAN_TYPE write(uint8_t header, uint8_t* buf, uint16_t length);
    uint16_t writeString(const char* string, uint8_t* buf, uint16_t pos);
    // Build up the header ready to send
    // Returns the size of the header
@@ -139,21 +144,21 @@ public:
    PubSubClient& setKeepAlive(uint16_t keepAlive);
    PubSubClient& setSocketTimeout(uint16_t timeout);
 
-   boolean setBufferSize(uint16_t size);
+   BOOLEAN_TYPE setBufferSize(uint16_t size);
    uint16_t getBufferSize();
 
-   boolean connect(const char* id);
-   boolean connect(const char* id, const char* user, const char* pass);
-   boolean connect(const char* id, const char* willTopic, uint8_t willQos, boolean willRetain, const char* willMessage);
-   boolean connect(const char* id, const char* user, const char* pass, const char* willTopic, uint8_t willQos, boolean willRetain, const char* willMessage);
-   boolean connect(const char* id, const char* user, const char* pass, const char* willTopic, uint8_t willQos, boolean willRetain, const char* willMessage, boolean cleanSession);
+   BOOLEAN_TYPE connect(const char* id);
+   BOOLEAN_TYPE connect(const char* id, const char* user, const char* pass);
+   BOOLEAN_TYPE connect(const char* id, const char* willTopic, uint8_t willQos, BOOLEAN_TYPE willRetain, const char* willMessage);
+   BOOLEAN_TYPE connect(const char* id, const char* user, const char* pass, const char* willTopic, uint8_t willQos, BOOLEAN_TYPE willRetain, const char* willMessage);
+   BOOLEAN_TYPE connect(const char* id, const char* user, const char* pass, const char* willTopic, uint8_t willQos, BOOLEAN_TYPE willRetain, const char* willMessage, BOOLEAN_TYPE cleanSession);
    void disconnect();
-   boolean publish(const char* topic, const char* payload);
-   boolean publish(const char* topic, const char* payload, boolean retained);
-   boolean publish(const char* topic, const uint8_t * payload, unsigned int plength);
-   boolean publish(const char* topic, const uint8_t * payload, unsigned int plength, boolean retained);
-   boolean publish_P(const char* topic, const char* payload, boolean retained);
-   boolean publish_P(const char* topic, const uint8_t * payload, unsigned int plength, boolean retained);
+   BOOLEAN_TYPE publish(const char* topic, const char* payload);
+   BOOLEAN_TYPE publish(const char* topic, const char* payload, BOOLEAN_TYPE retained);
+   BOOLEAN_TYPE publish(const char* topic, const uint8_t * payload, unsigned int plength);
+   BOOLEAN_TYPE publish(const char* topic, const uint8_t * payload, unsigned int plength, BOOLEAN_TYPE retained);
+   BOOLEAN_TYPE publish_P(const char* topic, const char* payload, BOOLEAN_TYPE retained);
+   BOOLEAN_TYPE publish_P(const char* topic, const uint8_t * payload, unsigned int plength, BOOLEAN_TYPE retained);
    // Start to publish a message.
    // This API:
    //   beginPublish(...)
@@ -162,7 +167,7 @@ public:
    // Allows for arbitrarily large payloads to be sent without them having to be copied into
    // a new buffer and held in memory at one time
    // Returns 1 if the message was started successfully, 0 if there was an error
-   boolean beginPublish(const char* topic, unsigned int plength, boolean retained);
+   BOOLEAN_TYPE beginPublish(const char* topic, unsigned int plength, BOOLEAN_TYPE retained);
    // Finish off this publish message (started with beginPublish)
    // Returns 1 if the packet was sent successfully, 0 if there was an error
    int endPublish();
@@ -171,11 +176,11 @@ public:
    // Write size bytes from buffer into the payload (only to be used with beginPublish/endPublish)
    // Returns the number of bytes written
    virtual size_t write(const uint8_t *buffer, size_t size);
-   boolean subscribe(const char* topic);
-   boolean subscribe(const char* topic, uint8_t qos);
-   boolean unsubscribe(const char* topic);
-   boolean loop();
-   boolean connected();
+   BOOLEAN_TYPE subscribe(const char* topic);
+   BOOLEAN_TYPE subscribe(const char* topic, uint8_t qos);
+   BOOLEAN_TYPE unsubscribe(const char* topic);
+   BOOLEAN_TYPE loop();
+   BOOLEAN_TYPE connected();
    int state();
 
 };
